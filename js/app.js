0d51f7f664b4b0e6d50165f0a0d4cb31f6ea3e54
RebeccaJ21 <rebecca.prows@gmail.com>
2018-01-09 21:58:28 -0800

    added two questions, siblings and travel

--- js/app.js
+++ js/app.js
@@ -18,3 +18,32 @@ alert('Correct! I have lived in Washington my whole life!');
     alert('Come on, it\'s a yes or no question, just take a guess next time!'); 
 }
 
+var siblings=prompt('Time for the next question! Do I have any siblings?').toLowerCase();
+console.log( 'User answered siblings with' + siblings);
+
+if (siblings==='y' || siblings==='yes') { 
+ var numberOfSiblings=prompt('You are right! Now for a bonus question can you guess how many siblings I have?');
+ if (parseInt(numberOfSiblings)===2 || numberOfSiblings==='two') {
+     alert('Wow! You got it! I have two sisters. They are 16.');
+ } else { 
+     alert('I actually have two siblings, 16 year old sisters!'); 
+ }
+
+ } else if (siblings==='n'|| siblings==="no") {
+    alert('Didn\'t get that one! I have two sisters!');
+} else {
+    alert('Seriously! It\'s a yes or no question, just take a guess next time!'); 
+}
+
+var travel=prompt('Have I traveled outside of the country?').toLowerCase();
+console.log( 'User answered travel with' + travel);
+
+if (travel==='y' || travel==='yes') { 
+alert('Good job! I have only been to Canada but that still counts and was a lot of fun! Hoping to go to Ireland at the end of this year!');
+} else if (travel==='n'|| travel==="no") {
+    alert('Actually I have! I\'ve only ever been to Canada, but it still counts!');
+} else {
+    alert('Since you didn\'t guess I guess I\'ll just tell you, I have been out of the country, I went all the way to Canada!'); 
+}
+
+
