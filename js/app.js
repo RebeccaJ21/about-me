73b5ea1806f91e6146011c76ccdb1cbaed30b421
Jeremy Crawford <34947155+JeremyCtown@users.noreply.github.com>
2018-01-11 14:36:01 -0800

    All questions wrapped in functions and all functions moved to the end of JS file. It works

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 'use strict';
-
-alert('Hello and Welcome To My Page! My name is Rebecca Prows, hope you enjoy learning a little bit about me!');
 var answersCorrect = 0;
+
+function openingGreeting() {
+  alert('Hello and Welcome To My Page! My name is Rebecca Prows, hope you enjoy learning a little bit about me!');
+}
 
 function questionsAboutUserName() {
   var userName = prompt('Before we get to me, please tell me your name!');
@@ -10,7 +12,7 @@ function questionsAboutUserName() {
   alert('Hello, ' + userName + ' thank you for stopping by! For the five yes or no questions please answer with Y or Yes, N or No. When you are ready hit OK!');
 
   var placesLived = prompt('Alright, ' + userName + ' let\'s start with something basic. Have I lived in any state beside Washington?').toLowerCase();
-  console.log( 'User answered placesLived with' + placesLived);
+  console.log( 'User answered placesLived with ' + placesLived);
 
   if (placesLived === 'n' || placesLived === 'no') { 
     alert('Correct! I have lived in Washington my whole life!');
@@ -21,12 +23,11 @@ function questionsAboutUserName() {
     alert('Come on, it\'s a yes or no question, just take a guess next time!'); 
   }
 }
-questionsAboutUserName();
 
 function questionSiblings() {
 
   var siblings = prompt('Time for the next question! Do I have any siblings?').toLowerCase();
-  console.log( 'User answered siblings with' + siblings);
+  console.log( 'User answered siblings with ' + siblings);
 
   if (siblings === 'y' || siblings === 'yes') { 
     var numberOfSiblings = prompt('You are right! Now for a bonus question can you guess how many siblings I have?').toLowerCase();
@@ -45,11 +46,9 @@ function questionSiblings() {
   }
 }
 
-questionSiblings();
-
 function questionTravel () {
   var travel = prompt('Have I traveled outside of the country?').toLowerCase();
-  console.log( 'User answered travel with' + travel);
+  console.log( 'User answered travel with ' + travel);
 
   if (travel === 'y' || travel === 'yes') { 
     alert('Good job! I have only been to Canada but that still counts and was a lot of fun! Hoping to go to Ireland at the end of this year!');
@@ -60,11 +59,10 @@ function questionTravel () {
     alert('Since you didn\'t guess I guess I\'ll just tell you, I have been out of the country, I went all the way to Canada!'); 
   }
 }
-questionTravel();
 
 function questionNickname() {
   var nickname = prompt('Do I have a nickname?').toLowerCase();
-  console.log('The user guessed' + nickname);
+  console.log('The user guessed ' + nickname);
 
   if (nickname === 'y' || nickname === 'yes') {
     alert('Yup! I mostly go by Rebecca but a lot of my friends call me RPB, I\'ll explain it if you ask. A few people call me Becca as well, but never Becky');
@@ -75,11 +73,10 @@ function questionNickname() {
     alert('Well, if you aren\'t going to ask then I am not going to tell you');
   }
 }
-questionNickname();
 
 function questionHairColor() {
   var hairColor = prompt('Is my hair it\'s natural color?').toLowerCase();
-  console.log('The user guessed' + hairColor);
+  console.log('The user guessed ' + hairColor);
 
   if (hairColor === 'y' || hairColor === 'yes') {
     alert('Correct!');
@@ -90,7 +87,6 @@ function questionHairColor() {
     alert('Spolier alert! It is my natural hair color'); 
   }
 }
-questionHairColor();
 
 function questionJob() {
   var jobYears = prompt('How many years have I worked at Safeway?').toLowerCase();
@@ -110,30 +106,48 @@ function questionJob() {
     alert('That was too high. The correct answer is 8.');
   }
   else{
-    console.log('User got correcr answer');
+    console.log('User got correct answer');
     alert('8 is correct!')
     answersCorrect += 1;
   } 
 }
-questionJob();
 
 function questionSports() {
-  var kidsSports = ['Swimming', 'Figure Skating', 'Soccer', 'T-Ball', 'Basketball'];
-  var kidsSportsGuess = prompt('Guess one of the sports I did as a kid').toLowerCase();
   var counter = 0;
-  for (var i = 0; i < kidsSports.length; i++); {
-    console.log('Incorrect sports guessed');
-    prompt('Incorrect! Guess again!');
-    counter += 1;
+  var kidsSports = ['swimming', 'figure skating', 'soccer', 't-ball', 'basketball'];
+  var flag = false;
   
-    if (kidsSportsGuess === kidsSports[i]) {
-      console.log('Correct Answer');
-      alert('You got it! I at some point participated in, Soccer, Swimming, T-Ball, Basketball, and Figure Skating!');
-      answersCorrect += 1;
-    } else {
-      alert('Nope! I at some point participated in, Soccer, Swimming, T-Ball, Basketball, and Figure Skating!');
+  while (counter < 6 && flag === false) {
+    var kidsSportsGuess = prompt('Guess one of the sports I did as a kid').toLowerCase();
+    counter ++;
+    for(var i = 0; i < kidsSports.length; i++) {
+      if(kidsSportsGuess === kidsSports[i]) {
+        alert('You got it! I at some point participated in Soccer, Swimming, T-Ball, Basketball, and Figure Skating!');
+        answersCorrect += 1;
+        flag = true;
+        console.log('Correct Answer');
+        break;
+      } 
+    }
+    if(flag === false) {
+      alert('Nope! Guess again!');
+    }
+    if(flag === false && kidsSportsGuess === 6) {
+      alert('All out of guesses! The answers are swimming, figure skating, soccer, t-ball, and basketball.');
     }
   }
 }
 
-alert('Hope you enjoyed getting to know a little bit about me! You got ' + answersCorrect + ' out of 8!');
+function closing() {
+  alert('Hope you enjoyed getting to know a little bit about me! You got ' + answersCorrect + ' out of 8!');
+}
+
+openingGreeting();
+questionsAboutUserName();
+questionSiblings();
+questionTravel();
+questionNickname();
+questionHairColor();
+questionJob();
+questionSports();
+closing();
